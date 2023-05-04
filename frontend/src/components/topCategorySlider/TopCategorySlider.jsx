d267f4c08cf9b4449397480b41745005140c8c00
foxxy <hoanganh6758@gmail.com>
2023-05-05 01:00:27 +0700

    implement TopCategorySlider component

---
+++ frontend/src/components/topCategorySlider/TopCategorySlider.jsx
@@ -0,0 +1,26 @@
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import styles from "./TopCategorySlider.module.css";
+
+const TopCategorySlider = ({ text }) => {
+    return (
+        <>
+            <div className={styles.top}>
+                <div className={styles.heading_cont}>
+                    <p className={styles.heading}>{text}</p>
+                    <MdOutlineKeyboardArrowRight className={styles.arrow} />
+                </div>
+                <div className={styles.icons}>
+                    <div className={styles.icon}>
+                        <RiArrowLeftSLine />
+                    </div>
+                    <div className={styles.icon}>
+                        <RiArrowRightSLine />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TopCategorySlider;
