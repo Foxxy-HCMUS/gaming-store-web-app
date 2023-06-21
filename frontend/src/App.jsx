92c3da2277967a1dc7738745063f2aaf46397d22
Miruku2201 <miruku2201@gmail.com>
2023-06-21 13:25:28 +0700

    merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -168,7 +168,7 @@ import { fetchUser } from "./store/slices/authSlice";
 import { Alert, Snackbar } from "@mui/material";
 import GamePage  from "./pages/GamePage/GamePage";
 // import theme from "./components/customTheme/customTheme";
-// import BrowsePage from './pages/BrowsePage/BrowsePage';
+import BrowsePage from './pages/BrowsePage/BrowsePage';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -247,7 +247,7 @@ export default function App() {
 
           <Route path="/register" element={<Register />} />
         
-          {/* <Route exact path="/browse" element={<BrowsePage />} /> */}
+          <Route exact path="/browse" element={<BrowsePage />} />
           
           {/* <Route exact path="/games/:id">
           <GamePage />
