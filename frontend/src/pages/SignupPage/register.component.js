5e5da7947710f498c5eac33aa723822cb3615b10
Marco Nguyen <bao1230802@gmail.com>
2023-07-03 19:38:29 +0700

    admin register

--- frontend/src/pages/SignupPage/register.component.js
+++ frontend/src/pages/SignupPage/register.component.js
@@ -1,214 +1,3 @@
-// import React, { Component } from "react";
-// import Form from "react-validation/build/form";
-// import Input from "react-validation/build/input";
-// import CheckButton from "react-validation/build/button";
-// import { isEmail } from "validator";
-
-// import AuthService from "../../services/auth.service";
-
-// const required = value => {
-//   if (!value) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         This field is required!
-//       </div>
-//     );
-//   }
-// };
-
-// const email = value => {
-//   if (!isEmail(value)) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         This is not a valid email.
-//       </div>
-//     );
-//   }
-// };
-
-// const vusername = value => {
-//   if (value.length < 3 || value.length > 20) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         The username must be between 3 and 20 characters.
-//       </div>
-//     );
-//   }
-// };
-
-// const vpassword = value => {
-//   if (value.length < 6 || value.length > 40) {
-//     return (
-//       <div className="alert alert-danger" role="alert">
-//         The password must be between 6 and 40 characters.
-//       </div>
-//     );
-//   }
-// };
-
-// class Register extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleRegister = this.handleRegister.bind(this);
-//     this.onChangeUsername = this.onChangeUsername.bind(this);
-//     this.onChangeEmail = this.onChangeEmail.bind(this);
-//     this.onChangePassword = this.onChangePassword.bind(this);
-
-//     this.state = {
-//       username: "",
-//       email: "",
-//       password: "",
-//       successful: false,
-//       message: ""
-//     };
-//   }
-
-//   onChangeUsername(e) {
-//     this.setState({
-//       username: e.target.value
-//     });
-//   }
-
-//   onChangeEmail(e) {
-//     this.setState({
-//       email: e.target.value
-//     });
-//   }
-
-//   onChangePassword(e) {
-//     this.setState({
-//       password: e.target.value
-//     });
-//   }
-
-//   handleRegister(e) {
-//     e.preventDefault();
-
-//     this.setState({
-//       message: "",
-//       successful: false
-//     });
-
-//     this.form.validateAll();
-
-//     if (this.checkBtn.context._errors.length === 0) {
-//       AuthService.register(
-//         this.state.username,
-//         this.state.email,
-//         this.state.password
-//       ).then(
-//         response => {
-//           this.setState({
-//             message: response.data.message,
-//             successful: true
-//           });
-//         },
-//         error => {
-//           const resMessage =
-//             (error.response &&
-//               error.response.data &&
-//               error.response.data.message) ||
-//             error.message ||
-//             error.toString();
-
-//           this.setState({
-//             successful: false,
-//             message: resMessage
-//           });
-//         }
-//       );
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="col-md-12">
-//         <div className="card card-container">
-//           <img
-//             src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-//             alt="profile-img"
-//             className="profile-img-card"
-//           />
-
-//           <Form
-//             onSubmit={this.handleRegister}
-//             ref={c => {
-//               this.form = c;
-//             }}
-//           >
-//             {!this.state.successful && (
-//               <div>
-//                 <div className="form-group">
-//                   <label htmlFor="username">Username</label>
-//                   <Input
-//                     type="text"
-//                     className="form-control"
-//                     name="username"
-//                     value={this.state.username}
-//                     onChange={this.onChangeUsername}
-//                     validations={[required, vusername]}
-//                   />
-//                 </div>
-
-//                 <div className="form-group">
-//                   <label htmlFor="email">Email</label>
-//                   <Input
-//                     type="text"
-//                     className="form-control"
-//                     name="email"
-//                     value={this.state.email}
-//                     onChange={this.onChangeEmail}
-//                     validations={[required, email]}
-//                   />
-//                 </div>
-
-//                 <div className="form-group">
-//                   <label htmlFor="password">Password</label>
-//                   <Input
-//                     type="password"
-//                     className="form-control"
-//                     name="password"
-//                     value={this.state.password}
-//                     onChange={this.onChangePassword}
-//                     validations={[required, vpassword]}
-//                   />
-//                 </div>
-
-//                 <div className="form-group">
-//                   <button className="btn btn-primary btn-block">Sign Up</button>
-//                 </div>
-//               </div>
-//             )}
-
-//             {this.state.message && (
-//               <div className="form-group">
-//                 <div
-//                   className={
-//                     this.state.successful
-//                       ? "alert alert-success"
-//                       : "alert alert-danger"
-//                   }
-//                   role="alert"
-//                 >
-//                   {this.state.message}
-//                 </div>
-//               </div>
-//             )}
-//             <CheckButton
-//               style={{ display: "none" }}
-//               ref={c => {
-//                 this.checkBtn = c;
-//               }}
-//             />
-//           </Form>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Register;
-
 import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
@@ -227,7 +16,12 @@ import { register } from "../../store/slices/authSlice";
 import { FormControlLabel } from "@mui/material";
 import AuthService from "../../services/auth.service";
 
-const Register = () => {
+export default function Register() {
+  const [userType, setUserType] = useState("");
+  const [secretKey, setSecretKey] = useState({
+    secretkey: "",
+    status: false,
+  });
   const user = useSelector((state) => state.auth.user);
   const [data, setData] = useState({
     username: "",
@@ -243,7 +37,28 @@ const Register = () => {
     firstName: "",
     lastName: "",
     password: "",
+    secretkey: "",
   });
+
+  const handleKeyInput = (e) => {
+    setSecretKey((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    })
+    const value = e.target.value;
+    if (value.length < 1) {
+      setErrors((prev) => ({ ...prev, secretkey: "Secret Key must not be left blank." }));
+    } else {
+      setErrors((prev) => ({ ...prev, secretkey: "" }));
+    }
+    if (value.length > 0) {
+      if (e.target.value === 'G02-admin') {
+        setSecretKey((prev) => ({ ...prev, status: true }));
+      }
+    }
+  }
 
   const handleInput = (e) => {
     setData((prev) => {
@@ -349,8 +164,7 @@ const Register = () => {
 
   const dispatch = useDispatch();
   let navigate = useNavigate();
-  // console.log(test);
-  // dispatch(register(test));
+
   const postUserData = () => {
     // dispatch(postUser(data));
     dispatch(register(data));
@@ -358,11 +172,48 @@ const Register = () => {
   };
 
   const [checked, setChecked] = useState(false);
-
+  
   return (
     <div className={styles.container}>
       <div className={styles.form}>
         <p className={styles.signup_text}>Sign Up</p>
+        <div> 
+          Register as:
+          <input
+            type="radio"
+            name="UserType"
+            value="User"
+            onChange={(e) => setUserType(e.target.value)}
+          />
+          User
+          <input
+            type="radio"
+            name="UserType"
+            value="Admin"
+            onChange={(e) => setUserType(e.target.value)}
+          />
+          Admin
+        </div>
+        
+        {userType === "Admin" ? (
+          <div>
+            <ThemeProvider theme={theme}>
+              <TextField
+                required
+                style={{ width: "100%" }}
+                label="Secret Key"
+                value={setSecretKey.secretkey}
+                // defaultValue={user ? user.secretkey : ''}
+                name="Secret Key"
+                type="text"
+                id="secretkey"
+                onChange={handleKeyInput}
+                error={errors.secretkey !== ""}
+                helperText={errors.secretkey}
+              />
+            </ThemeProvider>
+          </div>
+        ) : null}
         <div className={styles.names}>
           <ThemeProvider theme={theme}>
             <TextField
@@ -436,48 +287,53 @@ const Register = () => {
             helperText={errors.password}
           />
         </ThemeProvider>
-        <div className={styles.checkboxes}>
-          <div className={styles.checkbox}>
-            <Checkbox
-              sx={{
-                padding: 0,
+        {userType !== 'Admin' ? (
+          <div className={styles.checkboxes}>
+            <div className={styles.checkbox}>
+              <Checkbox
+                sx={{
+                  padding: 0,
+                  color: "gray",
+                  "& .MuiSvgIcon-root": { fontSize: 22 },
+                }}
+              />
+              <p>
+                I would like to receive news, surveys and special offers from Play Archive.
+              </p>
+            </div>
 
-                color: "gray",
-                "& .MuiSvgIcon-root": { fontSize: 22 },
-              }}
-            />
-            <p>
-              I would like to receive news, surveys and special offers from Play Archive.
-            </p>
-          </div>
-
-          <div className={styles.checkbox}>
-            <Checkbox
-              sx={{
-                padding: 0,
-                color: "gray",
-                "& .MuiSvgIcon-root": { fontSize: 22 },
-              }}
-              onClick={() => setChecked(!checked)}
-            />
-            <p>
-              I have read and agree to the{" "}
-              <span className={styles.underline}>terms of service</span>
-            </p>
-          </div>
-        </div>
-        <div>
-          <button
+            <div className={styles.checkbox}>
+              <Checkbox
+                sx={{
+                  padding: 0,
+                  color: "gray",
+                  "& .MuiSvgIcon-root": { fontSize: 22 },
+                }}
+                onClick={() => setChecked(!checked)}
+              />
+              <p>
+                I have read and agree to the{" "}
+                <span className={styles.underline}>terms of service</span>
+              </p>
+            </div>
+            <button
             onClick={(UserNameAvailability.status && checked) ? postUserData : null}
             style={(!UserNameAvailability.status || Object.values(errors).some((error) => error !== "") || !checked) ? { opacity: "0.5" } : null}
             className={styles.button}
           >
             CONTINUE
           </button>
-        </div>
+          </div>
+        ) : (
+        <button
+          onClick={((UserNameAvailability.status && secretKey.status)) ? postUserData : null}
+          style={(!UserNameAvailability.status || Object.values(errors).some((error) => error !== "") || !secretKey.status) ? { opacity: "0.5" } : null}
+          className={styles.button}
+        >
+          CONTINUE
+        </button>
+        )}
       </div>
-    </div>
+    </div>  
   );
-};
-
-export default Register;
+}
